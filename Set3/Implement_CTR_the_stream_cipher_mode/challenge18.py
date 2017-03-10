3911b0a8ce74f1b068ed8f0159558e765eda7d69
ryo18 <51303406@hcmut.edu.vn>
2017-03-10 16:39:22 +0700

    challenge

---
+++ Set3/Implement_CTR_the_stream_cipher_mode/challenge18.py
@@ -0,0 +1,42 @@
+import base64
+import struct
+from Crypto.Cipher import AES
+from Crypto.Util.strxor import strxor
+
+class CTR:
+	def __init__(self,ECB,nonce):
+		self._ECB = ECB
+		self._nonce = nonce
+		self._blocksize = 16
+		self._keybytes = b''
+		self._blockcount = 0
+	
+	def encrypt(self,plaintext):
+		if len(plaintext) == 0:
+			return b''
+
+		keystream = self._keybytes
+
+		while len(keystream) < len(plaintext):
+			keyblock = self._ECB.encrypt(struct.pack('<QQ', self._nonce, self._blockcount))
+			keystream += keyblock
+			self._blockcount += 1
+
+		if len(keystream) > len(plaintext):
+			self._keybytes = keystream[len(plaintext):]
+			keystream = keystream[:len(plaintext)]
+
+		return strxor(plaintext,keystream)
+
+	def decrypt(self,ciphertext):
+		return self.encrypt(ciphertext)
+
+
+if __name__ == '__main__':
+	a = base64.b64decode('L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ==')
+	key = b'YELLOW SUBMARINE'
+
+	cipher1 = CTR(AES.new(key,AES.MODE_ECB),0)
+	b = cipher1.decrypt(a)
+
+	print(b)
